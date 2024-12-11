4007d906c2b7858dcf3e99bc79bb0e9e203894d2
Geun Won Ma <wonny@Geunui-MacBookAir.local>
2024-12-11 18:03:32 +0900

    Fix : fileFieldsInterceptor로 변경해서 실습

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -9,7 +9,7 @@ import { Role } from 'src/user/entities/user.entity';
 import { GetMoviesDto } from './dto/get-movies.dto';
 import { CacheInterceptor } from 'src/common/interceptor/cache.interceptor';
 import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('movie')
 @UseInterceptors(ClassSerializerInterceptor) // class transfomer를 MovieController에 적용하겠다는 것.
@@ -36,11 +36,25 @@ export class MovieController {
   @Post()
   @RBAC(Role.admin) // 이건 admin만 접근 가능 하다는 것
   @UseInterceptors(TransactionInterceptor) // queryRunner를 붙이기 위함
-  @UseInterceptors(FilesInterceptor('movies')) // post 할 때 파일 업로드를 같이 진행함. 'movie'는 파일 올릴 때 키값 이름임, FileInterceptor는 파일 한개만 업로드할 때 사용하고 FilesInterceptor는 여러개 파일 업로드 할 때 사용
+  // @UseInterceptors(FilesInterceptor('movies')) // post 할 때 파일 업로드를 같이 진행함. 'movie'는 파일 올릴 때 키값 이름임, FileInterceptor는 파일 한개만 업로드할 때 사용하고 FilesInterceptor는 여러개 파일 업로드 할 때 사용
+  @UseInterceptors(FileFieldsInterceptor([ // 파일 필드를 두개 이상 받을 때는 FileFieldsInterceptor를 사용함. 리스트로 받아야 함
+    {
+      name: 'movie', // 어떤 필드로 받을 것인지
+      maxCount: 1, // 최대 몇개 받을 것인지 
+    },
+    {
+      name: 'poster',
+      maxCount: 2,
+    }
+  ]))
   postMovie(
    @Body() body: CreateMovieDto,
    @Request() req,
-   @UploadedFiles() files: Express.Multer.File[], // 파일 형식이 express의 multer file 형식이라는 것. 여기도 파일 한개면 UploadedFile을 사용하고 여러개 일땐 UploadedFiles를 사용해야 함. 파일 여러개 일 땐 리스트[]로 받아야 하기 때문에 써줘야 함 
+  //  @UploadedFiles() files: Express.Multer.File[], // 파일 형식이 express의 multer file 형식이라는 것. 여기도 파일 한개면 UploadedFile을 사용하고 여러개 일땐 UploadedFiles를 사용해야 함. 파일 여러개 일 땐 리스트[]로 받아야 하기 때문에 써줘야 함 
+  @UploadedFiles() files: { // FileFieldsInterceptor 사용할 때는 객체로 묶어서 각각 필드에 설정을 해줘야 함
+    movie?: Express.Multer.File[],
+    poster?: Express.Multer.File[],
+  } 
   ){
     console.log('------------------');
     console.log(files);
